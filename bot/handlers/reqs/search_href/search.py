1ff2e1156451ab7f26e52c1081df092178d6fef6
n3ke2710 <vinogradov.na@phystech.edu>
2025-05-19 22:21:06 +0300

    search updated

--- bot/handlers/reqs/search_href/search.py
+++ bot/handlers/reqs/search_href/search.py
@@ -3,7 +3,7 @@ from googlesearch import search # type: ignore
 
 def search_first_result(movie_name, is_series: bool = False):
 	if is_series:
-		query = f"Сериал {movie_name} смотреть онлайн site:yandex.ru/video"
+		query = f"Сериал {movie_name} смотреть онлайн"
 	else:
 		query = f"Фильм {movie_name} смотреть онлайн site:vk.com | site:rutube.ru"
 	for url in search(query, num_results=1, lang="ru"):
